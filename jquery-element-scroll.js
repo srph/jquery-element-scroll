ace60dc47e6eff952e3c2c00613ddf99a35a7d28
srph <seraphipod@gmail.com>
2014-06-02 21:56:09 +0800

    v0.5: Cleaned code and changes to variable names. Added an underscore prefix to variables I reckoned to be of importance. Documented. Thinking of revising code base or pattern. However, with a small library such as this, imo, an overkill.

--- jquery-element-scroll.js
+++ jquery-element-scroll.js
@@ -12,7 +12,7 @@
  *
  * Thinking of renaming this library to 'jquery-div-scroller'
  *
- * @version 0.4.1
+ * @version 0.5
  * @todo 	tests
  * 			elements other than 100% divs
  *
@@ -23,27 +23,27 @@
  */
 
 (function($) {
-	$.fn.elmScroll = function(speed) {
-		// Body element
-		var body = $(document.body);
-
-		// Window element
-		var win = $(window);
-
+	/**
+	 * {@inherit}
+	 *
+	 * @param 	integer 	speed
+	 * @return 	void
+	 */
+	var elementScroll = function(speed) {
 		// Elements to scroll to
-		var elms = this; 
+		var _elms = this;
+
+		// Default settings
+		var _default = { speed: 400 };
+
+		// Custom settings passed to the function
+		var _custom = { speed: speed };
 
 		// Fn settings
-		var settings = $.extend({
-			speed: 400
-		}, {
-			speed: parseInt(speed, 10)
-		});
-        
-        // Starts with the first element
-        // Zero by default since screen starts
-        // with the zero/first element
-       var elm = 0;
+		var _settings = $.extend(_default, _custom);
+
+		// Body element
+		var body = $(document.body);
 			
 		body.on('DOMMouseScroll mousewheel', function (e) {
 			// Event data
@@ -51,55 +51,102 @@
 			var wheelDelta = e.originalEvent.wheelDelta;
 
 			// Scroll direction
-			// 1: up
-			// 0: down
-			var dir = ( detail > 0 || wheelDelta < 0 )
-				? 0
-				: 1;
+			var _dir = getDirection(detail, wheelDelta);
 
 			// Currently visible div
 			// Default by negative one to reset
 			// *....
-			var elm = -1;
-
-			// Iterate through each element
-			elms.each(function( index ) {
-				var current = $(this),
-					offset = current.offset();
-
-				// Window current top
-				var winTop = win.scrollTop();
-
-				// Determine the index of the current element
-				// Apparently, If the body is currently not on
-				// the first element, set it to the current
-				if ( elm < 0 && offset.top >= winTop ) {
-					elm = index;
-				}
-			});
-
-			// If direction is upwards and
-			// the current element is negative
-			if ( dir == 1 && elm > 0 ) {
-				elm--;
-			}
-
-			// If direction is downwards
-			// and as long the current element is not the last element
-			// Animate the screen downwards towards the next element
-			if ( dir == 0 && elm < elms.length - 1 ) {
-				elm++;
-			}
+			var curElm = getCurrentElm(_elms, _dir);
+			var nextElm = getNextElm(curElm, _elms, _dir);
 
 			// Stop current screen animation
 			// then animate by given speed
 			$('html, body').stop().animate({
-				scrollTop: elms.eq(elm).offset().top
-			}, settings.speed);
+				scrollTop: _elms.eq(nextElm).offset().top
+			}, _settings.speed);
 
 			// Avoid flickering and stop event
 			// IDK lel
 			return false;
 		});
 	}
+
+	/**
+	 * Return scroll direction
+	 * 1: up
+	 * 0: down
+	 *
+	 * @return 	bool|int
+	 */
+	var getDirection = function(detail, delta) {
+		return ( detail > 0 || delta < 0 )
+			? 0
+			: 1;
+	}
+
+	/**
+	 * Get the current element where the window is
+	 *
+	 * @param 	$ 		elms
+	 * @param 	int 	dir
+	 * @return 	int
+	 */
+	var getCurrentElm = function(elms, dir) {
+		// Body element
+		var body = $(document.body);
+
+		// Window element
+		var win = $(window);
+
+		// Reset the element to negative one to
+		// assure the dom that we are not on the first element
+		var elm = -1;
+
+		// Iterate through each element
+		elms.each(function( index ) {
+			var current = $(this),
+				offset = current.offset();
+
+			// Window current top
+			var winTop = win.scrollTop();
+
+			// Determine the index of the current element
+			// Apparently, If the body is currently not on
+			// the first element, set it to the current
+			if ( elm < 0 && offset.top >= winTop ) {
+				elm = index;
+			}
+		});
+
+		return elm;
+	}
+
+	/**
+	 * Get next element
+	 *
+	 * @param 	int	elm
+	 * @param 	$ 	elms
+	 * @param 	int dir
+	 * @return 	int
+	 */
+	var getNextElm = function(elm, elms, dir) {
+		// If direction is both upwards and
+		// the current element index precedes the first element,
+		// continue scrolling upwards
+		if ( dir == 1 && elm > 0 ) {
+			elm--;
+		}
+
+		// If direction is downwards
+		// and as long the current element is not the last element
+		// Animate the screen downwards towards the next element
+		if ( dir == 0 && elm < elms.length - 1 ) {
+			elm++;
+		}
+
+		return elm;
+	}
+
+	// Register fn
+	$.fn.elmScroll = elementScroll;
 })(jQuery);
